48202a97ebe61c7ba1b45226cea9c72761e64f4a
tusqasi <2002.tushar.kuntawar@gmail.com>
2020-12-12 15:44:55 +0530

    sever working condition commit

--- flaskapp.py
+++ flaskapp.py
@@ -3,13 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/home")
-def home():
-    return render_template('home.html')
+@app.route("/home/<var>")
+def home(var="hi"):
+    return render_template('home.html',title=var)
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html',)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
