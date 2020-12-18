f93ca4fb64f4ef666e99a8680c70deb7ef37023a
tusqasi <2002.tushar.kuntawar@gmail.com>
2020-12-18 15:27:32 +0530

    Added CSS and forms

--- flaskapp.py
+++ flaskapp.py
@@ -1,23 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for   
+from form import RegForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'dd1435ed1e2298ddda0594ac7d2699a0'
 
 posts = [
         {
             'title': "First post",
             'author': "tusqasi",
-            'date_posted': "December, 12th, 2020"
+            'date_posted': "December, 12th, 2020",
+            'content': "Is this working"
         },
 
         {
             'title': "Second post",
             'author': "tusqasi",
-            'date_posted': "December, 14th, 2020"
+            'date_posted': "December, 14th, 2020",
+            'content': "hi There"
         },
         {
             'title': "Third post",
             'author': "tusqasi",
-            'date_posted': "December, 15th, 2020"
+            'date_posted': "December, 15th, 2020",
+            'content': "you still here"
         }
     ]
 
@@ -29,6 +35,16 @@ def home(var="hi", posts=posts):
 @app.route("/about")
 def about():
     return render_template('about.html',)
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
